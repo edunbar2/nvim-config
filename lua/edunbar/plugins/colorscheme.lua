fa010781b921dc1ad42104ea394e0bd2e03f8516
edunbar2 <eric@dunbar.net>
2024-03-03 18:44:24 -0500

    Updated theme to work with 8 bit colors in macOS terminal

--- lua/edunbar/plugins/colorscheme.lua
+++ lua/edunbar/plugins/colorscheme.lua
@@ -1,7 +1,11 @@
 return{
-   'AlexvZyl/nordic.nvim',
-   name = "nordic",
+   --'AlexvZyl/nordic.nvim',
+   'junegunn/seoul256.vim',
+   --name = "theme",
+   name = "soul256",
    config = function()
-	vim.cmd("colorscheme nordic")
+	vim.cmd("colorscheme seoul256")
+	vim.api.nvim_set_hl(0, "Normal", { background = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { background = "none" })
    end,
-} 
+}
