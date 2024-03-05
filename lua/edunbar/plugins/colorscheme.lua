777d5df5473fc36251e55bd63a84ce5bceec8c86
Eric Dunbar <69920313+edunbar2@users.noreply.github.com>
2024-03-05 09:35:11 -0500

    Update colorscheme.lua
    Removed MacOS color scheme and replaced with oxocarbon theme.

--- lua/edunbar/plugins/colorscheme.lua
+++ lua/edunbar/plugins/colorscheme.lua
@@ -1,11 +1,9 @@
 return{
    --'AlexvZyl/nordic.nvim',
-   --"nyoom-engineering/oxocarbon.nvim",
---	name = "oxocarbon",
-"jdsimcoe/abstract.vim",
-	name='abstract',
+   "nyoom-engineering/oxocarbon.nvim",
+	name = "oxocarbon",
    config = function()
-	vim.cmd("colorscheme abstract")
+	vim.cmd("colorscheme oxocarbon")
 	vim.api.nvim_set_hl(0, "Normal", { background = "none" })
 	vim.api.nvim_set_hl(0, "NormalFloat", { background = "none" })
    end,
